dcd978a0ab39099930b08245355d315fa5731f74
Dhruvil <kasodariyadhruvil45@gmail.com>
2025-01-31 12:36:31 +0530

    debug

--- index.js
+++ index.js
@@ -56,6 +56,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-});listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
 });
