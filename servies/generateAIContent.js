ff28d96dc0389e152b3866fdb50bd08d6b182b3c
Dhruvil <kasodariyadhruvil45@gmail.com>
2025-01-31 11:24:29 +0530

    cretae chat theret

--- servies/generateAIContent.js
+++ servies/generateAIContent.js
@@ -6,9 +6,20 @@ require('dotenv').config();
 async function generateAIContent(prompt = "give 200 word essay about cow") {
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+  });
   try {
-    const result = await model.generateContent([prompt]);
+    const result = await chat.startChat(prompt);
     return result.response.text();
   } catch (error) {
     console.error("Error generating AI content:", error);
